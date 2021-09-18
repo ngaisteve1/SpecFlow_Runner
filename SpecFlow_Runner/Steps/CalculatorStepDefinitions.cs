da2201352c10c8587d7f1ba9360f58c7f665f907
ngaisteve1 <findingsteve@gmail.com>
2021-09-18 17:26:35 +0800

    Add project files.

---
+++ SpecFlow_Runner/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using SpecFlow_Service;
+using TechTalk.SpecFlow;
+
+namespace SpecFlow_Runner.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+        private Calculator calculator;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+            calculator = new Calculator();
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            calculator.Number1 = number;
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            calculator.Number2 = number;
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            calculator.Add();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            result.Should().Be(calculator.Result);
+        }
+    }
+}
---
+++ SpecFlow_Service/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace SpecFlow_Service
+{
+    public class Calculator
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+        public int Result { get; set; }
+
+        public void Add() {
+            Result = Number1 + Number2;
+        }
+    }
+}
